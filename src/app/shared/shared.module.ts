1f0f970e2e042bb2d59bf93be7f9e0801676d132
syedaslam <ammuaslam689@gmail.com>
2019-07-14 15:15:54 +0530

    home page work started.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule, MatIconModule, BrowserAnimationsModule
   ]
 })
 export class SharedModule { }
