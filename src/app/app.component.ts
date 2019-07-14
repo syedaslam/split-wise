1854ca4b0c9def1003615c29c1a0359974def1b6
syedaslam <ammuaslam689@gmail.com>
2019-07-14 23:09:25 +0530

    sidenave completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'split-wise';
+  toggle: (boolean) = false;
+
+  list = [
+    {
+      name: 'Home',
+      link: 'home-page'
+    },
+    {
+      name: 'Settings',
+      link: 'home-page'
+    },
+    {
+      name: 'Log out',
+      link: 'home-page'
+    }
+  ]
 }
