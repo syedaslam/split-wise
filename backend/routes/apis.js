0def8f405b14f13670f3090ffad4f1c8524449e0
syedaslam <ammuaslam689@gmail.com>
2019-08-18 18:35:25 +0530

    delete API issue resolved

--- backend/routes/apis.js
+++ backend/routes/apis.js
@@ -68,7 +68,8 @@ router.delete('/:id', (req, res, next) => {
 })
 
 router.delete('/group/:id', (req, res, next) => {
-  apiData.deleteOne({_id: req.params.id}).then( result => {
+  console.log(req.params);
+  groupData.deleteOne({_id: req.params.id}).then( result => {
     res.status(200).json({message: 'Deleted successfully!'});
   })
 })
